dd617d152f6d5a7dfc762020703d649ef3cdf87a
gator-boi <gator-boi@users.noreply.github.com>
2023-09-26 10:27:43 -0500

    evm: repo setup

---
+++ evm/src/LiquidityLayer.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity 0.8.19;
+
+import {BytesParsing} from "wormhole-solidity/WormholeBytesParsing.sol";
+
+contract LiquidityLayer {
+	using BytesParsing for bytes;
+}
