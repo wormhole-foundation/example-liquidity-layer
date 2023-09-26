cdf127a8904bdb12aab49570f160b68e271dcd5d
gator-boi <gator-boi@users.noreply.github.com>
2023-09-26 15:51:42 -0500

    evm: add liquidity to curve pool in forge test

--- evm/forge-test/CurvePool.sol
+++ evm/forge-test/CurvePool.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
-interface curveFactory {
+interface CurveFactory {
 	function deploy_plain_pool(
 		string memory _name,
 		string memory _symbol,
@@ -17,29 +17,25 @@ interface curveFactory {
 	) external returns (address);
 }
 
+interface CurvePool {
+	function add_liquidity(
+		uint256[4] memory _amounts,
+		uint256 _min_mint_amount
+	) external returns (uint256);
+}
+
 contract WormholeCurvePool {
 	address constant curveFactoryAddress =
 		0xb17b674D9c5CB2e441F8e196a2f048A81355d031;
-	address constant usdc = 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E;
-	address constant ethUsdc = 0xB24CA28D4e2742907115fECda335b40dbda07a4C;
-	address constant solUsdc = 0x0950Fc1AD509358dAeaD5eB8020a3c7d8b43b9DA;
-	address constant polyUsdc = 0x543672E9CBEC728CBBa9C3Ccd99ed80aC3607FA8;
-	address curvePool;
+	address immutable curvePool;
 
-	constructor() {
-		// Wormhole USDCs
-		address[4] memory coins;
-		coins[0] = usdc;
-		coins[1] = ethUsdc;
-		coins[2] = solUsdc;
-		coins[3] = polyUsdc;
-
+	constructor(address[4] memory coins) {
 		uint256 A = 100;
 		uint256 fee = 4000000;
 		uint256 asset_type = 0;
 		uint256 implementation_idx = 0;
 
-		curvePool = curveFactory(curveFactoryAddress).deploy_plain_pool(
+		curvePool = CurveFactory(curveFactoryAddress).deploy_plain_pool(
 			"Mothership Test Pool", // Name
 			"WormUSDC", // Symbol
 			coins,
@@ -50,7 +46,10 @@ contract WormholeCurvePool {
 		);
 	}
 
-	function poolAddress() public view returns (address) {
-		return curvePool;
+	function addCurveLiquidity(
+		uint256[4] memory amounts,
+		uint256 minLPAmount
+	) internal returns (uint256) {
+		return CurvePool(curvePool).add_liquidity(amounts, minLPAmount);
 	}
 }
--- evm/forge-test/Mothership.t.sol
+++ evm/forge-test/Mothership.t.sol
@@ -3,20 +3,45 @@
 pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
+import "forge-std/StdUtils.sol";
 import "forge-std/console.sol";
 
 import {WormholeCurvePool} from "./CurvePool.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract MothershipTest is Test {
-	WormholeCurvePool pool;
+contract MothershipTest is Test, WormholeCurvePool {
+	address constant usdc = 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E;
+	address constant ethUsdc = 0xB24CA28D4e2742907115fECda335b40dbda07a4C;
+	address constant solUsdc = 0x0950Fc1AD509358dAeaD5eB8020a3c7d8b43b9DA;
+	address constant polyUsdc = 0x543672E9CBEC728CBBa9C3Ccd99ed80aC3607FA8;
+	address[4] poolCoins;
 
 	/// @notice We use a constructor here so that the curve pool is
 	/// only deployed once (vs. in a `setUp` function).
-	constructor() {
-		pool = new WormholeCurvePool();
+	constructor() WormholeCurvePool([usdc, ethUsdc, solUsdc, polyUsdc]) {
+		poolCoins = [usdc, ethUsdc, solUsdc, polyUsdc];
 	}
 
-	function testCurvePool() public {
-		assertTrue(pool.poolAddress() != address(0));
+	function mintAndProvideLiquidity(uint256 amount) internal {
+		// Mint tokens and approve them for the curve pool.
+		uint256[4] memory amounts;
+		for (uint256 i = 0; i < poolCoins.length; ++i) {
+			deal(poolCoins[i], address(this), amount);
+			IERC20(poolCoins[i]).approve(curvePool, amount);
+			amounts[i] = amount;
+		}
+
+		addCurveLiquidity(
+			amounts,
+			0 // minimum LP shares
+		);
+	}
+
+	function setUp() public {
+		mintAndProvideLiquidity(5000000 * 10 ** 6);
+	}
+
+	function testDeez() public {
+		return;
 	}
 }
