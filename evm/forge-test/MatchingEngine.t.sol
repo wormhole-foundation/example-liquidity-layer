737ba22b891d287377c56344418988416d55ed90
gator-boi <gator-boi@users.noreply.github.com>
2023-09-29 17:38:34 -0500

    evm: add unit tests for admin methods

--- evm/forge-test/MatchingEngine.t.sol
+++ evm/forge-test/MatchingEngine.t.sol
@@ -9,6 +9,7 @@ import "forge-std/console.sol";
 import {IMatchingEngine} from "../src/interfaces/IMatchingEngine.sol";
 import {WormholeCurvePool} from "./CurvePool.sol";
 import {MatchingEngine} from "../src/MatchingEngine/MatchingEngine.sol";
+import {ICurvePool} from "curve-solidity/ICurvePool.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MatchingEngineTest is Test, WormholeCurvePool {
@@ -128,5 +129,234 @@ contract MatchingEngineTest is Test, WormholeCurvePool {
 		vm.prank(makeAddr("robber"));
 		vm.expectRevert(abi.encodeWithSignature("NotTheOwner()"));
 		engine.enableExecutionRoute(chainId, target, cctp, poolIndex);
+	}
+
+	function testDisableExecutionRoute() public {
+		uint16 chainId = 69;
+		address target = makeAddr("ethEmitter");
+		bool cctp = true;
+		int8 poolIndex = 1;
+
+		// Set the initial route.
+		engine.enableExecutionRoute(chainId, target, cctp, poolIndex);
+
+		// Disable the route.
+		engine.disableExecutionRoute(chainId);
+
+		IMatchingEngine.Route memory route = engine.getExecutionRoute(chainId);
+		assertEq(route.target, address(0));
+		assertEq(route.cctp, false);
+		assertEq(route.poolIndex, 0);
+	}
+
+	function testCannotDisableExecutionRoute() public {
+		uint16 chainId = 69;
+
+		vm.prank(makeAddr("robber"));
+		vm.expectRevert(abi.encodeWithSignature("NotTheOwner()"));
+		engine.disableExecutionRoute(chainId);
+	}
+
+	function testRegisterOrderRouter() public {
+		uint16 chainId = 69;
+		bytes32 router = bytes32(uint256(420));
+
+		{
+			bytes32 registered = engine.getOrderRouter(chainId);
+			assertEq(registered, bytes32(0));
+		}
+
+		// Set the initial router.
+		{
+			engine.registerOrderRouter(chainId, router);
+
+			bytes32 registered = engine.getOrderRouter(chainId);
+			assertEq(registered, router);
+		}
+
+		// Update the router to make sure the owner can change it.
+		{
+			router = bytes32(uint256(69));
+
+			engine.registerOrderRouter(chainId, router);
+
+			bytes32 registered = engine.getOrderRouter(chainId);
+			assertEq(registered, router);
+		}
+	}
+
+	function testCannotRegisterOrderRouterInvalidAddress() public {
+		uint16 chainId = 69;
+		bytes32 router = bytes32(uint256(0));
+
+		vm.expectRevert(abi.encodeWithSignature("InvalidAddress()"));
+		engine.registerOrderRouter(chainId, router);
+	}
+
+	function testCannotRegisterOrderRouterInvalidChainId() public {
+		uint16 chainId = 0;
+		bytes32 router = bytes32(uint256(420));
+
+		vm.expectRevert(abi.encodeWithSignature("InvalidChainId()"));
+		engine.registerOrderRouter(chainId, router);
+	}
+
+	function testCannotRegisterOrderRouterOwnerOnly() public {
+		uint16 chainId = 69;
+		bytes32 router = bytes32(uint256(420));
+
+		vm.prank(makeAddr("robber"));
+		vm.expectRevert(abi.encodeWithSignature("NotTheOwner()"));
+		engine.registerOrderRouter(chainId, router);
+	}
+
+	function testUpdateCurvePool() public {
+		// Check initial curve pool info.
+		{
+			IMatchingEngine.CurvePoolInfo memory info = engine.getCurvePoolInfo();
+			assertEq(address(info.pool), curvePool);
+			assertEq(info.nativeTokenIndex, 0);
+		}
+
+		// Update the curve pool.
+		{
+			ICurvePool newCurvePool = ICurvePool(makeAddr("newCurvePool"));
+			int8 newNativeTokenIndex = 1;
+
+			engine.updateCurvePool(newCurvePool, newNativeTokenIndex);
+
+			IMatchingEngine.CurvePoolInfo memory info = engine.getCurvePoolInfo();
+			assertEq(address(info.pool), address(newCurvePool));
+			assertEq(info.nativeTokenIndex, newNativeTokenIndex);
+		}
+	}
+
+	function testCannotUpdateCurvePoolInvalidAddress() public {
+		ICurvePool newCurvePool = ICurvePool(address(0));
+		int8 newNativeTokenIndex = 1;
+
+		vm.expectRevert(abi.encodeWithSignature("InvalidAddress()"));
+		engine.updateCurvePool(newCurvePool, newNativeTokenIndex);
+	}
+
+	function testCannotUpdateCurvePoolOwnerOnly() public {
+		ICurvePool newCurvePool = ICurvePool(makeAddr("newCurvePool"));
+		int8 newNativeTokenIndex = 1;
+
+		vm.prank(makeAddr("robber"));
+		vm.expectRevert(abi.encodeWithSignature("NotTheOwner()"));
+		engine.updateCurvePool(newCurvePool, newNativeTokenIndex);
+	}
+
+	function testSetPause() public {
+		// Check initial pause state.
+		{
+			bool paused = engine.getPaused();
+			assertEq(paused, false);
+		}
+
+		// Pause the contract.
+		{
+			engine.setPause(true);
+
+			bool paused = engine.getPaused();
+			assertEq(paused, true);
+		}
+
+		// Unpause the contract.
+		{
+			engine.setPause(false);
+
+			bool paused = engine.getPaused();
+			assertEq(paused, false);
+		}
+	}
+
+	function testSubmitOwnershipTransferRequest() public {
+		address newOwner = makeAddr("newOwner");
+
+		// Check initial ownership state.
+		{
+			address owner = engine.getOwner();
+			assertEq(owner, address(this));
+
+			address pendingOwner = engine.getPendingOwner();
+			assertEq(pendingOwner, address(0));
+		}
+
+		// Submit the ownership transfer request.
+		{
+			engine.submitOwnershipTransferRequest(newOwner);
+
+			address pendingOwner = engine.getPendingOwner();
+			assertEq(pendingOwner, newOwner);
+		}
+	}
+
+	function testCannotSubmitOwnershipTransferRequestInvalidAddress() public {
+		address newOwner = address(0);
+
+		vm.expectRevert(abi.encodeWithSignature("InvalidAddress()"));
+		engine.submitOwnershipTransferRequest(newOwner);
+	}
+
+	function testCannotSubmitOwnershipTransferRequestOwnerOnly() public {
+		address newOwner = makeAddr("newOwner");
+
+		vm.prank(makeAddr("robber"));
+		vm.expectRevert(abi.encodeWithSignature("NotTheOwner()"));
+		engine.submitOwnershipTransferRequest(newOwner);
+	}
+
+	function testCancelOwnershipTransferRequest() public {
+		address newOwner = makeAddr("newOwner");
+
+		// Submit the ownership transfer request.
+		engine.submitOwnershipTransferRequest(newOwner);
+
+		// Cancel the ownership transfer request.
+		engine.cancelOwnershipTransferRequest();
+
+		address pendingOwner = engine.getPendingOwner();
+		assertEq(pendingOwner, address(0));
+	}
+
+	function testCannotCancelOwnershipTransferRequestOwnerOnly() public {
+		address newOwner = makeAddr("newOwner");
+
+		// Submit the ownership transfer request.
+		engine.submitOwnershipTransferRequest(newOwner);
+
+		vm.prank(makeAddr("robber"));
+		vm.expectRevert(abi.encodeWithSignature("NotTheOwner()"));
+		engine.cancelOwnershipTransferRequest();
+	}
+
+	function testConfirmOwnershipTransferRequest() public {
+		address newOwner = makeAddr("newOwner");
+
+		// Verify current owner.
+		assertEq(engine.getOwner(), address(this));
+
+		// Submit the ownership transfer request.
+		engine.submitOwnershipTransferRequest(newOwner);
+
+		// Confirm by pranking with the newOwner address.
+		vm.prank(newOwner);
+		engine.confirmOwnershipTransferRequest();
+
+		assertEq(engine.getOwner(), newOwner);
+		assertEq(engine.getPendingOwner(), address(0));
+	}
+
+	function testCannotConfirmOwnershipTransferRequestNotPendingOwner() public {
+		address newOwner = makeAddr("newOwner");
+
+		// Submit the ownership transfer request.
+		engine.submitOwnershipTransferRequest(newOwner);
+
+		vm.prank(makeAddr("robber"));
+		vm.expectRevert(abi.encodeWithSignature("NotPendingOwner()"));
+		engine.confirmOwnershipTransferRequest();
 	}
 }
