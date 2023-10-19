bc2d97c73550fc85b16b293106497620d6675165
gator-boi <gator-boi@users.noreply.github.com>
2023-10-19 11:27:48 -0500

    evm: implement Nativeswap

--- examples/nativeswap/src/NativeSwapV2.sol
+++ examples/nativeswap/src/NativeSwapV2.sol
@@ -9,291 +9,149 @@ import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 import {BytesLib} from "wormhole-solidity/BytesLib.sol";
 import {IWormhole} from "wormhole-solidity/IWormhole.sol";
-//import {IOrderRouter} from "liquidity-layer/interfaces/IOrderRouter.sol";
+import {IOrderRouter, PlaceMarketOrderArgs, OrderResponse, RedeemedFill} from "liquidity-layer/interfaces/IOrderRouter.sol";
+import {fromUniversalAddress} from "liquidity-layer/shared/Utils.sol";
 import {IWETH} from "wormhole-solidity/IWETH.sol";
-import {Messages} from "./Messages.sol";
 
-/// @title A cross-chain UniswapV2 example
-/// @notice Swaps against UniswapV2 pools and uses Wormhole's Liquidity Layer integration.
-// contract CrossChainSwapV2 is Messages {
-//     using SafeERC20 for IERC20;
-//     using BytesLib for bytes;
+import {NativeSwapBase} from "./NativeSwapBase.sol";
 
-//     // contract deployer
-//     address public immutable deployer;
+contract NativeSwapV2 is NativeSwapBase {
+    using SafeERC20 for IERC20;
+    using BytesLib for bytes;
 
-//     // contracts
-//     IUniswapV2Router02 public immutable SWAP_ROUTER;
-//     IWormhole public immutable WORMHOLE;
-//     IOrderRouter public immutable ORDER_ROUTER;
+    IUniswapV2Router02 public immutable SWAP_ROUTER;  
 
-//     // token addresses
-//     address public immutable USDC_ADDRESS;
-//     address public immutable WRAPPED_NATIVE_ADDRESS;
+    constructor(
+        address _swapRouterAddress,
+        address _wormholeAddress,
+        address _orderRouterAddress,
+        address _usdcAddress,
+        address _wrappedNativeAddress
+    ) NativeSwapBase(_wormholeAddress, _orderRouterAddress, _usdcAddress, _wrappedNativeAddress) {
+        SWAP_ROUTER = IUniswapV2Router02(_swapRouterAddress);
+    }
 
-//     // registered nativeswap contracts
-//     mapping(uint16 => bytes32) registeredContracts;
+    /// @dev Calls _swapExactInBeforeTransfer and encodes custom payload with
+    /// instructions for executing native asset swaps on the destination chain
+    function swapExactNativeInAndTransfer(
+        ExactInParameters calldata swapParams,
+        address[] calldata path,
+        uint16 targetChainId, 
+        uint256 wormholeSlippage
+    ) external payable { 
+        (bytes32 targetContract, uint256 targetChainRelayerFee) = _verifyInput(
+            path, 
+            swapParams.amountOutMinimum, 
+            targetChainId, 
+            wormholeSlippage
+        );
 
-//     constructor(
-//         address _swapRouterAddress,
-//         address _wormholeAddress,
-//         address _orderRouterAddress,
-//         address _usdcAddress,
-//         address _wrappedNativeAddress
-//     ) {
-//         deployer = msg.sender;
-//         SWAP_ROUTER = IUniswapV2Router02(_swapRouterAddress);
-//         WORMHOLE = IWormhole(_wormholeAddress);
-//         ORDER_ROUTER = IOrderRouter(_orderRouterAddress);
-//         USDC_ADDRESS = _usdcAddress;
-//         WRAPPED_NATIVE_ADDRESS = _wrappedNativeAddress;
-//     }
+        // cache wormhole fee and check msg.value
+        uint256 wormholeFee = WORMHOLE.messageFee();
+        require(msg.value > WORMHOLE.messageFee(), "insufficient value");
 
-//     /// @dev Used to communicate information about executed swaps to UI/user
-//     event SwapResult(
-//         address indexed _recipient,
-//         address _tokenOut,
-//         address _from,
-//         uint256 _amountOut,
-//         uint8 _success
-//     );
+        // wrap native asset
+        IWETH(WRAPPED_NATIVE_ADDRESS).deposit{
+            value : msg.value - wormholeFee
+        }();
 
-//     /// @dev Calls _swapExactInBeforeTransfer and encodes custom payload with
-//     /// instructions for executing native asset swaps on the destination chain
-//     function swapExactNativeInAndTransfer(
-//         ExactInParameters calldata swapParams,
-//         address[] calldata path,
-//         uint256 relayerFee,
-//         uint16 targetChainId,
-//         bytes32 targetContractAddress
-//     ) external payable {
-//         require(
-//             swapParams.amountOutMinimum > relayerFee,
-//             "insufficient amountOutMinimum to pay relayer"
-//         );
-//         require(
-//             path[0]==WRAPPED_NATIVE_ADDRESS,
-//             "tokenIn must be wrapped native asset"
-//         );
-//         require(
-//             path[1]==USDC_ADDRESS,
-//             "tokenOut must be USDC"
-//         );
-//         require(path.length == 4, "invalid path");
-//         require(
-//             registeredContracts[targetChainId] == targetContractAddress,
-//             "target contract not registered"
-//         );
+        // `amountOut` is the USDC amount that was received from the swap
+        uint256 amountOut = _swapExactInBeforeTransfer(
+            msg.value - wormholeFee,
+            swapParams.amountOutMinimum,
+            path[0:2],
+            swapParams.deadline
+        );
 
-//         // cache wormhole fee and check msg.value
-//         uint256 wormholeFee = WORMHOLE.messageFee();
-//         require(msg.value > WORMHOLE.messageFee(), "insufficient value");
+        // approve USDC integration contract to spend USDC
+        SafeERC20.safeIncreaseAllowance(
+            IERC20(USDC_ADDRESS),
+            address(ORDER_ROUTER),
+            amountOut
+        );
 
-//         // wrap native asset
-//         IWETH(WRAPPED_NATIVE_ADDRESS).deposit{
-//             value : msg.value - wormholeFee
-//         }();
+        ORDER_ROUTER.placeMarketOrder{value: wormholeFee}(
+            PlaceMarketOrderArgs({
+                amountIn: amountOut,
+                minAmountOut: amountOut - wormholeSlippage, 
+                targetChain: targetChainId,
+                redeemer: targetContract,
+                redeemerMessage: encodeSwapInParameters(swapParams, path, targetChainRelayerFee),
+                refundAddress: msg.sender
+            })
+        );
+    }
 
-//         // peform the first swap
-//         uint256 amountOut = _swapExactInBeforeTransfer(
-//             msg.value - wormholeFee,
-//             swapParams.amountOutMinimum,
-//             path[0:2],
-//             swapParams.deadline
-//         );
+    function _swapExactInBeforeTransfer(
+        uint256 amountIn,
+        uint256 amountOutMinimum,
+        address[] calldata path,
+        uint256 deadline
+    ) internal returns (uint256 amountOut) {
+        // approve the router to spend tokens
+        SafeERC20.safeIncreaseAllowance(
+            IERC20(path[0]),
+            address(SWAP_ROUTER),
+            amountIn
+        );
 
-//         // create payload with target swap instructions
-//         bytes memory payload = abi.encodePacked(
-//             uint8(1),
-//             swapParams.targetAmountOutMinimum,
-//             swapParams.targetChainRecipient,
-//             path[2],
-//             path[3],
-//             swapParams.deadline,
-//             swapParams.poolFee,
-//             relayerFee
-//         );
+        // perform the swap
+        uint256[] memory amounts = SWAP_ROUTER.swapExactTokensForTokens(
+            amountIn,
+            amountOutMinimum,
+            path,
+            address(this),
+            deadline
+        );
+        amountOut = amounts[1];
+    }
 
-//         // approve USDC integration contract to spend USDC
-//         SafeERC20.safeIncreaseAllowance(
-//             IERC20(USDC_ADDRESS),
-//             address(ORDER_ROUTER),
-//             amountOut
-//         );
+    /// @dev Mints USDC and executes exactIn native asset swap and pays the relayer
+    function recvAndSwapExactNativeIn(
+        OrderResponse calldata orderResponse
+    ) external payable returns (uint256[] memory amounts) {
+        (
+            RecvSwapInParameters memory swapParams, 
+            uint256 swapAmount
+        ) = _handleAndVerifyFill(orderResponse); 
 
-//         ORDER_ROUTER.placeMarketOrder(
-//             IOrderRouter.PlaceMarketOrderArgs({
-//                 amountIn: amountOut,
-//                 minAmountOut: swapParams.targetAmountOutMinimum,
-//                 targetChain: targetChainId,
-//                 redeemer: targetContractAddress,
-//                 redeemerMessage: payload,
-//                 refundAddress: msg.sender
-//             }),
-//             relayerFee,
-//             new bytes32[](0)
-//         );
-//     }
+        // create dynamic address array, uniswap won't take fixed size array
+        address[] memory uniPath = new address[](2);
+        uniPath[0] = swapParams.path[0];
+        uniPath[1] = swapParams.path[1]; 
 
-//     function _swapExactInBeforeTransfer(
-//         uint256 amountIn,
-//         uint256 amountOutMinimum,
-//         address[] calldata path,
-//         uint256 deadline
-//     ) internal returns (uint256 amountOut) {
-//         // approve the router to spend tokens
-//         SafeERC20.safeApprove(
-//             IERC20(path[0]),
-//             address(SWAP_ROUTER),
-//             amountIn
-//         );
+        // convert recipient bytes32 address to type address
+        address recipientAddress = fromUniversalAddress(swapParams.recipientAddress);
 
-//         // perform the swap
-//         uint256[] memory amounts = SWAP_ROUTER.swapExactTokensForTokens(
-//             amountIn,
-//             amountOutMinimum,
-//             path,
-//             address(this),
-//             deadline
-//         );
-//         amountOut = amounts[1];
-//     }
+        // approve the router to spend tokens
+        SafeERC20.safeIncreaseAllowance(
+            IERC20(uniPath[0]),
+            address(SWAP_ROUTER),
+            swapAmount
+        );
 
-//     /// @dev Mints USDC and executes exactIn native asset swap and pays the relayer
-//     function recvAndSwapExactNativeIn(
-//         ICircleIntegration.RedeemParameters memory redeemParams
-//     ) external payable returns (uint256[] memory amounts) {
-//         // check USDC balance before minting
-//         uint256 balanceBefore = IERC20(USDC_ADDRESS).balanceOf(address(this));
-
-//         // mint USDC to this contract
-//         ICircleIntegration.DepositWithPayload memory deposit = ORDER_ROUTER.redeemTokensWithPayload(
-//             redeemParams
-//         );
-
-//         // check USDC balance after minting
-//         uint256 balanceAfter = IERC20(USDC_ADDRESS).balanceOf(address(this));
-//         uint256 swapAmount = balanceAfter - balanceBefore;
-
-//         // parse swap params from USDC integration contract payload
-//         RecvSwapInParameters memory swapParams = decodeSwapInParameters(
-//             deposit.payload
-//         );
-
-//         // verify that the sender is a registered contract
-//         require(
-//             deposit.fromAddress == registeredContracts[
-//                 ORDER_ROUTER.getChainIdFromDomain(deposit.sourceDomain)
-//             ],
-//             "fromAddress is not a registered contract"
-//         );
-
-//         // create dynamic address array, uniswap won't take fixed size array
-//         address[] memory uniPath = new address[](2);
-//         uniPath[0] = swapParams.path[0];
-//         uniPath[1] = swapParams.path[1];
-
-//         // sanity check path
-//         require(
-//             uniPath[0]==USDC_ADDRESS &&
-//             address(uint160(uint256(deposit.token)))==USDC_ADDRESS,
-//             "tokenIn must be USDC"
-//         );
-//         require(
-//             uniPath[1]==WRAPPED_NATIVE_ADDRESS,
-//             "tokenOut must be wrapped native asset"
-//         );
-
-//         // approve the router to spend tokens
-//         SafeERC20.safeApprove(
-//             IERC20(uniPath[0]),
-//             address(SWAP_ROUTER),
-//             swapAmount
-//         );
-
-//         // convert recipient bytes32 address to type address
-//         address recipientAddress = address(uint160(uint256(swapParams.recipientAddress)));
-
-//         // try to execute the swap
-//         try SWAP_ROUTER.swapExactTokensForTokens(
-//             swapAmount,
-//             swapParams.estimatedAmount,
-//             uniPath,
-//             address(this),
-//             swapParams.deadline
-//         ) returns (uint256[] memory amounts) {
-//             // calculate how much to pay the relayer in the native token
-//             uint256 nativeRelayerFee = amounts[1] * swapParams.relayerFee / swapAmount;
-//             uint256 nativeAmountOut = amounts[1] - nativeRelayerFee;
-
-//             // unwrap native and send to recipient
-//             IWETH(WRAPPED_NATIVE_ADDRESS).withdraw(amounts[1]);
-//             payable(recipientAddress).transfer(nativeAmountOut);
-
-//             /// pay the relayer in the native token
-//             payable(msg.sender).transfer(nativeRelayerFee);
-
-//             // used in UI to tell user they're getting
-//             // their desired token
-//             emit SwapResult(
-//                 recipientAddress,
-//                 uniPath[1],
-//                 msg.sender,
-//                 nativeAmountOut,
-//                 1
-//             );
-//             return amounts;
-//         } catch {
-//             // pay relayer in the USDC since the swap failed
-//             IERC20 feeToken = IERC20(USDC_ADDRESS);
-//             feeToken.safeTransfer(msg.sender, swapParams.relayerFee);
-
-//             // swap failed - return USDC to recipient
-//             feeToken.safeTransfer(
-//                 recipientAddress,
-//                 swapAmount - swapParams.relayerFee
-//             );
-
-//             // set the Uniswap allowance to zero
-//              SafeERC20.safeApprove(
-//                 IERC20(swapParams.path[0]),
-//                 address(SWAP_ROUTER),
-//                 0
-//             );
-
-//             // used in UI to tell user theyere getting
-//             // USDC instead of their desired native asset
-//             emit SwapResult(
-//                 recipientAddress,
-//                 uniPath[0],
-//                 msg.sender,
-//                 swapAmount - swapParams.relayerFee,
-//                 0
-//             );
-//         }
-//     }
-
-//     /// @dev `registerContract` serves to save trusted circle relayer contract addresses
-//     function registerContract(
-//         uint16 chainId,
-//         bytes32 contractAddress
-//     ) public onlyDeployer {
-//         // sanity check both input arguments
-//         require(
-//             contractAddress != bytes32(0),
-//             "emitterAddress cannot equal bytes32(0)"
-//         );
-//         require(chainId != 0, "chainId must be > 0");
-
-//         // update the registeredContracts state variable
-//         registeredContracts[chainId] = contractAddress;
-//     }
-
-//     modifier onlyDeployer() {
-//         require(deployer == msg.sender, "caller not the deployer");
-//         _;
-//     }
-
-//     // necessary for receiving native assets
-//     receive() external payable {}
-// }
+        // try to execute the swap
+        try SWAP_ROUTER.swapExactTokensForTokens(
+            swapAmount,
+            swapParams.estimatedAmount,
+            uniPath,
+            address(this),
+            swapParams.deadline
+        ) returns (uint256[] memory amounts) {
+            _handleSuccessfulSwap(
+                amounts[1], 
+                swapAmount, 
+                swapParams.relayerFee, 
+                recipientAddress
+            );
+            return amounts;
+        } catch {
+            _handleFailedSwap(
+                swapAmount, 
+                swapParams.relayerFee, 
+                recipientAddress,
+                address(SWAP_ROUTER)
+            );
+        }
+    }  
+}
---
+++ examples/nativeswap/src/NativeSwapV3.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+import {BytesLib} from "wormhole-solidity/BytesLib.sol";
+import {IWormhole} from "wormhole-solidity/IWormhole.sol";
+import {IOrderRouter, PlaceMarketOrderArgs, OrderResponse, RedeemedFill} from "liquidity-layer/interfaces/IOrderRouter.sol";
+import {fromUniversalAddress} from "liquidity-layer/shared/Utils.sol";
+import {IWETH} from "wormhole-solidity/IWETH.sol";
+import "./interfaces/IUniswap.sol";
+
+import {NativeSwapBase} from "./NativeSwapBase.sol";
+
+contract NativeSwapV3 is NativeSwapBase {
+    using SafeERC20 for IERC20;
+    using BytesLib for bytes;
+
+    IUniswapRouter public immutable SWAP_ROUTER;
+
+    constructor(
+        address _swapRouterAddress,
+        address _wormholeAddress,
+        address _orderRouterAddress,
+        address _usdcAddress,
+        address _wrappedNativeAddress
+    ) NativeSwapBase(_wormholeAddress, _orderRouterAddress, _usdcAddress, _wrappedNativeAddress) {
+        SWAP_ROUTER = IUniswapRouter(_swapRouterAddress);
+    }
+
+    /// @dev Calls _swapExactInBeforeTransfer and encodes custom payload with
+    /// instructions for executing native asset swaps on the destination chain
+    function swapExactNativeInAndTransfer(
+        ExactInParameters calldata swapParams,
+        address[] calldata path,
+        uint16 targetChainId,
+        uint256 wormholeSlippage
+    ) external payable {
+        (bytes32 targetContract, uint256 targetChainRelayerFee) = _verifyInput(
+            path, 
+            swapParams.amountOutMinimum, 
+            targetChainId, 
+            wormholeSlippage
+        );
+
+        // cache wormhole fee and check msg.value
+        uint256 wormholeFee = WORMHOLE.messageFee();
+        require(msg.value > WORMHOLE.messageFee(), "insufficient value");
+
+        // peform the first swap
+        uint256 amountOut = _swapExactInBeforeTransfer(
+            msg.value - wormholeFee,
+            swapParams.amountOutMinimum,
+            path[0:2],
+            swapParams.deadline,
+            swapParams.poolFee
+        );
+
+        // approve USDC integration contract to spend USDC
+        SafeERC20.safeIncreaseAllowance(
+            IERC20(USDC_ADDRESS),
+            address(ORDER_ROUTER),
+            amountOut
+        );
+
+        ORDER_ROUTER.placeMarketOrder{value: wormholeFee}(
+            PlaceMarketOrderArgs({
+                amountIn: amountOut,
+                minAmountOut: amountOut - wormholeSlippage, 
+                targetChain: targetChainId,
+                redeemer: targetContract,
+                redeemerMessage: encodeSwapInParameters(swapParams, path, targetChainRelayerFee),
+                refundAddress: msg.sender
+            })
+        );
+    }
+
+    function _swapExactInBeforeTransfer(
+        uint256 amountIn,
+        uint256 amountOutMinimum,
+        address[] calldata path,
+        uint256 deadline,
+        uint24 poolFee
+    ) internal returns (uint256 amountOut) {
+        // set swap options with user params
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: path[0],
+                tokenOut: path[1],
+                fee: poolFee,
+                recipient: address(this),
+                deadline: deadline,
+                amountIn: amountIn,
+                amountOutMinimum: amountOutMinimum,
+                sqrtPriceLimitX96: 0
+            });
+
+        // perform the swap
+        amountOut = SWAP_ROUTER.exactInputSingle{value: amountIn}(params);
+    }
+
+    /// @dev Mints USDC and executes exactIn native asset swap and pays the relayer
+    function recvAndSwapExactNativeIn(
+        OrderResponse calldata orderResponse
+    ) external returns (uint256 amountOut) {
+        (
+            RecvSwapInParameters memory swapParams,
+            uint256 swapAmount
+        ) = _handleAndVerifyFill(orderResponse); 
+
+        // convert recipient bytes32 address to type address
+        address recipientAddress = fromUniversalAddress(swapParams.recipientAddress);
+
+        // approve the router to spend tokens
+        SafeERC20.safeIncreaseAllowance(
+            IERC20(swapParams.path[0]),
+            address(SWAP_ROUTER),
+            swapAmount
+        ); 
+
+        // try to execute the swap
+        try SWAP_ROUTER.exactInputSingle(
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: swapParams.path[0],
+                tokenOut: swapParams.path[1],
+                fee: swapParams.poolFee,
+                recipient: address(this),
+                deadline: swapParams.deadline,
+                amountIn: swapAmount,
+                amountOutMinimum: swapParams.estimatedAmount,
+                sqrtPriceLimitX96: 0
+            })
+        ) returns (uint256 amountOut) {
+            _handleSuccessfulSwap(
+                amountOut, 
+                swapAmount, 
+                swapParams.relayerFee, 
+                recipientAddress
+            );
+            return amountOut;
+        } catch {
+            _handleFailedSwap(
+                swapAmount, 
+                swapParams.relayerFee, 
+                recipientAddress, 
+                address(SWAP_ROUTER)
+            );
+        }
+    }
+}
