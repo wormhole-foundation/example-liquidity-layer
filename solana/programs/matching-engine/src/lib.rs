5d5171b9df5cf704fc118c4d8920111765e9e867
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-04-24 11:49:36 -0500

    solana: fix close proposal comment

--- solana/programs/matching-engine/src/lib.rs
+++ solana/programs/matching-engine/src/lib.rs
@@ -176,8 +176,8 @@ pub mod matching_engine {
         processor::update_auction_parameters(ctx)
     }
 
-    /// This instruction is used to close an existing proposal by closing the propsal account. This
-    /// instruction can only be called by the `owner`.
+    /// This instruction is used to close an existing proposal by closing the proposal account. This
+    /// instruction can only be called by the `owner` or `owner_assistant`.
     /// # Arguments
     ///
     /// * `ctx` - `CloseProposal` context.
