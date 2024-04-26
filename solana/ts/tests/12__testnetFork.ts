88adf7f53e30c6cfec9e7472a451236047f9bddb
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-04-26 17:44:22 -0500

    solana: add comment

--- solana/ts/tests/12__testnetFork.ts
+++ solana/ts/tests/12__testnetFork.ts
@@ -24,6 +24,7 @@ chaiUse(chaiAsPromised);
 const MATCHING_ENGINE_ARTIFACT_PATH = `${__dirname}/artifacts/new_testnet_matching_engine.so`;
 const TOKEN_ROUTER_ARTIFACT_PATH = `${__dirname}/artifacts/new_testnet_token_router.so`;
 
+// TODO: We need to run the solana test validator differently in order for these to work.
 describe.skip("Upgrade Manager", function () {
     const connection = new Connection(LOCALHOST, "processed");
     const payer = PAYER_KEYPAIR;
