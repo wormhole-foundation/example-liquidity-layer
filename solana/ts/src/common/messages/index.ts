7ab956cfcdde544e77828115818d74c0890ed895
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-05-15 09:22:48 -0400

    Add layout definitions

--- solana/ts/src/common/messages/index.ts
+++ solana/ts/src/common/messages/index.ts
@@ -1,5 +1,14 @@
+import { fastMarketOrderLayout } from "@wormhole-foundation/example-liquidity-layer-definitions";
+import {
+    ChainId,
+    UniversalAddress,
+    deserializeLayout,
+    isChain,
+    serializeLayout,
+    toChain,
+    toChainId,
+} from "@wormhole-foundation/sdk";
 import { ID_DEPOSIT, LiquidityLayerDeposit } from "./deposit";
-import { ChainId, isChainId } from "@wormhole-foundation/sdk";
 
 export * from "./deposit";
 
@@ -33,9 +42,7 @@ export class LiquidityLayerMessage {
     }
 
     static decode(buf: Buffer): LiquidityLayerMessage {
-        let offset = 0;
-        const payloadId = buf.readUInt8(offset);
-        offset += 1;
+        const payloadId = buf.readUInt8(0);
 
         let deposit: LiquidityLayerDeposit | undefined;
         let fastMarketOrder: FastMarketOrder | undefined;
@@ -46,29 +53,7 @@ export class LiquidityLayerMessage {
                 break;
             }
             case ID_FAST_MARKET_ORDER: {
-                const amountIn = buf.readBigUInt64BE(offset);
-                offset += 8;
-                const minAmountOut = buf.readBigUInt64BE(offset);
-                offset += 8;
-                const targetChain = buf.readUInt16BE(offset);
-                if (!isChainId(targetChain)) {
-                    throw new Error("Invalid target chain");
-                }
-                offset += 2;
-                const redeemer = Array.from(buf.subarray(offset, (offset += 32)));
-                const sender = Array.from(buf.subarray(offset, (offset += 32)));
-                const refundAddress = Array.from(buf.subarray(offset, (offset += 32)));
-                const maxFee = buf.readBigUInt64BE(offset);
-                offset += 8;
-                const initAuctionFee = buf.readBigUInt64BE(offset);
-                offset += 8;
-                const deadline = buf.readUInt32BE(offset);
-                offset += 4;
-                const redeemerMessageLen = buf.readUInt16BE(offset);
-                offset += 2;
-                const redeemerMessage = buf.subarray(offset, (offset += redeemerMessageLen));
-
-                fastMarketOrder = {
+                const {
                     amountIn,
                     minAmountOut,
                     targetChain,
@@ -79,6 +64,22 @@ export class LiquidityLayerMessage {
                     initAuctionFee,
                     deadline,
                     redeemerMessage,
+                } = deserializeLayout(fastMarketOrderLayout, new Uint8Array(buf));
+                if (!isChain(targetChain)) {
+                    throw new Error("Invalid target chain");
+                }
+
+                fastMarketOrder = {
+                    amountIn,
+                    minAmountOut,
+                    targetChain: toChainId(targetChain),
+                    redeemer: Array.from(redeemer.toUint8Array()),
+                    sender: Array.from(sender.toUint8Array()),
+                    refundAddress: Array.from(refundAddress.toUint8Array()),
+                    maxFee,
+                    initAuctionFee,
+                    deadline,
+                    redeemerMessage: Buffer.from(redeemerMessage),
                 };
                 break;
             }
@@ -110,27 +111,20 @@ export class LiquidityLayerMessage {
                     redeemerMessage,
                 } = fastMarketOrder;
 
-                const messageBuf = Buffer.alloc(1 + 136 + redeemerMessage.length);
+                const messageBuf = serializeLayout(fastMarketOrderLayout, {
+                    amountIn,
+                    minAmountOut,
+                    targetChain: toChain(targetChain),
+                    redeemer: new UniversalAddress(new Uint8Array(redeemer)),
+                    sender: new UniversalAddress(new Uint8Array(sender)),
+                    refundAddress: new UniversalAddress(new Uint8Array(refundAddress)),
+                    maxFee: maxFee,
+                    initAuctionFee: initAuctionFee,
+                    deadline: deadline,
+                    redeemerMessage: new Uint8Array(redeemerMessage),
+                });
 
-                let offset = 0;
-                offset = messageBuf.writeUInt8(ID_FAST_MARKET_ORDER, offset);
-                offset = messageBuf.writeBigUInt64BE(amountIn, offset);
-                offset = messageBuf.writeBigUInt64BE(minAmountOut, offset);
-                offset = messageBuf.writeUInt16BE(targetChain, offset);
-                messageBuf.set(redeemer, offset);
-                offset += redeemer.length;
-                messageBuf.set(sender, offset);
-                offset += sender.length;
-                messageBuf.set(refundAddress, offset);
-                offset += refundAddress.length;
-                offset = messageBuf.writeBigUInt64BE(maxFee, offset);
-                offset = messageBuf.writeBigUInt64BE(initAuctionFee, offset);
-                offset = messageBuf.writeUInt32BE(deadline, offset);
-                offset = messageBuf.writeUInt16BE(redeemerMessage.length, offset);
-                messageBuf.set(redeemerMessage, offset);
-                offset += redeemerMessage.length;
-
-                return messageBuf;
+                return Buffer.from(messageBuf);
             } else {
                 throw new Error("Invalid Liquidity Layer message");
             }
---
+++ universal/ts/src/layouts.ts
@@ -0,0 +1,51 @@
+import { Layout, layoutItems } from "@wormhole-foundation/sdk";
+
+export const wormholeCctpDepositHeaderLayout = [
+    layoutItems.payloadIdItem(1),
+    { name: "token", ...layoutItems.universalAddressItem },
+    { name: "amount", ...layoutItems.amountItem },
+    { name: "sourceDomain", ...layoutItems.circleDomainItem },
+    { name: "targetDomain", ...layoutItems.circleDomainItem },
+    { name: "nonce", ...layoutItems.circleNonceItem },
+    { name: "fromAddress", ...layoutItems.universalAddressItem },
+    { name: "mintRecipient", ...layoutItems.universalAddressItem },
+    { name: "payload", binary: "bytes", lengthSize: 2 },
+] as const satisfies Layout;
+
+// Common properties for Fill types
+const basefillLayout = [
+    { name: "sourceChain", ...layoutItems.chainItem() },
+    { name: "orderSender", ...layoutItems.universalAddressItem },
+    { name: "redeemer", ...layoutItems.universalAddressItem },
+    { name: "redeemerMessage", binary: "bytes", lengthSize: 2 },
+] as const satisfies Layout;
+
+export const fillLayout = [
+    layoutItems.payloadIdItem(1),
+    ...basefillLayout,
+] as const satisfies Layout;
+
+export const fastFillLayout = [
+    layoutItems.payloadIdItem(12),
+    { name: "fillAmount", binary: "uint", size: 8 },
+    ...basefillLayout,
+] as const satisfies Layout;
+
+export const fastMarketOrderLayout = [
+    layoutItems.payloadIdItem(11),
+    { name: "amountIn", binary: "uint", size: 8 },
+    { name: "minAmountOut", binary: "uint", size: 8 },
+    { name: "targetChain", ...layoutItems.chainItem() },
+    { name: "redeemer", ...layoutItems.universalAddressItem },
+    { name: "sender", ...layoutItems.universalAddressItem },
+    { name: "refundAddress", ...layoutItems.universalAddressItem },
+    { name: "maxFee", binary: "uint", size: 8 },
+    { name: "initAuctionFee", binary: "uint", size: 8 },
+    { name: "deadline", binary: "uint", size: 4 },
+    { name: "redeemerMessage", binary: "bytes", lengthSize: 2 },
+] as const satisfies Layout;
+
+export const slowOrderResponseLayout = [
+    layoutItems.payloadIdItem(2),
+    { name: "baseFee", binary: "uint", size: 8 },
+] as const satisfies Layout;
