f10bc74bbd8002ee4918c84b916d879d5459d98e
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-06-03 11:09:32 -0500

    solana: same change for token router

--- solana/programs/token-router/src/state/prepared_fill.rs
+++ solana/programs/token-router/src/state/prepared_fill.rs
@@ -53,14 +53,14 @@ pub struct PreparedFill {
 impl PreparedFill {
     pub const SEED_PREFIX: &'static [u8] = b"fill";
 
-    pub fn checked_compute_size(payload_len: usize) -> Option<usize> {
+    pub fn compute_size(payload_len: usize) -> usize {
         const FIXED: usize = 8 // DISCRIMINATOR
             + PreparedFillSeeds::INIT_SPACE
             + PreparedFillInfo::INIT_SPACE
             + 4 // payload len
         ;
 
-        payload_len.checked_add(FIXED)
+        payload_len.saturating_add(FIXED)
     }
 }
 
