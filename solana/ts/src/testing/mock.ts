539b7d5f449328254f9b40be8f8eac7299e663b2
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-06-18 20:11:05 -0400

    replace postVaa with method that takes signer and no guardian args

--- solana/ts/src/testing/mock.ts
+++ solana/ts/src/testing/mock.ts
@@ -1,12 +1,68 @@
 import { Connection, Keypair } from "@solana/web3.js";
 import { ethers } from "ethers";
 import { LiquidityLayerMessage } from "../common";
-import { CORE_BRIDGE_PID, GUARDIAN_KEY } from "./consts";
+import { CORE_BRIDGE_PID, GUARDIAN_KEY, MOCK_GUARDIANS } from "./consts";
 import { postVaa, getBlockTime } from "./utils";
 import { mocks } from "@wormhole-foundation/sdk-definitions/testing";
-import { utils as coreUtils } from "@wormhole-foundation/sdk-solana-core";
-import { Chain } from "@wormhole-foundation/sdk-base";
-import { serialize, toUniversal } from "@wormhole-foundation/sdk-definitions";
+import { signAndSendWait } from "@wormhole-foundation/sdk-connect";
+import { SolanaWormholeCore, utils as coreUtils } from "@wormhole-foundation/sdk-solana-core";
+import { Chain, Network, contracts } from "@wormhole-foundation/sdk-base";
+import {
+    SignAndSendSigner,
+    buildConfig,
+    serialize,
+    toUniversal,
+} from "@wormhole-foundation/sdk-definitions";
+import { SolanaAddress, SolanaSendSigner } from "@wormhole-foundation/sdk-solana";
+import { VaaAccount } from "../wormhole";
+
+export type SDKSigner<N extends Network> = SolanaSendSigner<N, "Solana">;
+
+export function getSdkSigner<N extends Network>(
+    connection: Connection,
+    key: Keypair,
+    debug: boolean = false,
+): { signer: SDKSigner<N>; address: SolanaAddress } {
+    const signer = new SolanaSendSigner(connection, "Solana", key, debug, {});
+    const address = new SolanaAddress(key.publicKey);
+    return { signer, address };
+}
+
+// TODO: return VaaAccount, too
+export async function postLiquidityLayerVaav2(
+    connection: Connection,
+    payer: Keypair | SignAndSendSigner<Network, "Solana">,
+    foreignEmitterAddress: Array<number>,
+    sequence: bigint,
+    message: LiquidityLayerMessage | Buffer,
+    args: { sourceChain?: Chain; timestamp?: number } = {},
+) {
+    let { sourceChain, timestamp } = args;
+    sourceChain ??= "Ethereum";
+    timestamp ??= await getBlockTime(connection);
+
+    const foreignEmitter = new mocks.MockEmitter(
+        toUniversal(sourceChain, new Uint8Array(foreignEmitterAddress)),
+        sourceChain ?? "Ethereum",
+        sequence - 1n,
+    );
+
+    const published = foreignEmitter.publishMessage(
+        0, // nonce,
+        Buffer.isBuffer(message) ? message : message.encode(),
+        0, // consistencyLevel
+        timestamp,
+    );
+
+    const vaa = MOCK_GUARDIANS.addSignatures(published, [0]);
+
+    await postVaa(connection, payer, Buffer.from(serialize(vaa)));
+
+    const address = coreUtils.derivePostedVaaKey(CORE_BRIDGE_PID, Buffer.from(vaa.hash));
+    const account = await VaaAccount.fetch(connection, address);
+
+    return { address, account };
+}
 
 // TODO: return VaaAccount, too
 export async function postLiquidityLayerVaa(
