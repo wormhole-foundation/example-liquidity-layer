530598ab6ec3cddb59c853a4f53d5e265623b190
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-06-25 09:52:29 -0400

    tmp before migrating ethers version

--- evm/ts/src/protocol/tokenRouter.ts
+++ evm/ts/src/protocol/tokenRouter.ts
@@ -1,19 +1,25 @@
 import { TokenRouter } from "@wormhole-foundation/example-liquidity-layer-definitions";
-import { Chain, Network } from "@wormhole-foundation/sdk-base";
+import { Network, nativeChainIds, toChainId } from "@wormhole-foundation/sdk-base";
 import {
-    AccountAddress,
-    UnsignedTransaction,
-    VAA,
     CircleBridge,
     Contracts,
+    UnsignedTransaction,
+    VAA,
 } from "@wormhole-foundation/sdk-definitions";
-import { EvmTokenRouter as _EvmTokenRouter } from "../TokenRouter";
+import {
+    AnyEvmAddress,
+    EvmAddress,
+    EvmChains,
+    EvmUnsignedTransaction,
+} from "@wormhole-foundation/sdk-evm";
 import { ethers } from "ethers";
+import { EvmTokenRouter as _EvmTokenRouter } from "../TokenRouter";
 
-export class EvmTokenRouter<N extends Network, C extends Chain>
+export class EvmTokenRouter<N extends Network, C extends EvmChains>
     extends _EvmTokenRouter
     implements TokenRouter<N, C>
 {
+    private _chainId: number;
     constructor(
         readonly network: N,
         readonly chain: C,
@@ -21,16 +27,32 @@ export class EvmTokenRouter<N extends Network, C extends Chain>
         readonly contracts: Contracts & TokenRouter.Addresses,
     ) {
         super(provider, contracts.tokenRouter, contracts.cctp.tokenMessenger);
+        this._chainId = 0; //nativeChainIds.networkChainToNativeChainId(network, chain);
     }
 
     async *placeMarketOrder(
-        sender: AccountAddress<C>,
-        order: TokenRouter.OrderRequest | TokenRouter.PreparedOrder<C>,
+        sender: AnyEvmAddress,
+        order: TokenRouter.OrderRequest,
     ): AsyncGenerator<UnsignedTransaction<N, C>, any, unknown> {
-        throw new Error("Method not implemented.");
+        const msg = order.redeemerMessage ? order.redeemerMessage : new Uint8Array();
+
+        const refundAddress = order.refundAddress
+            ? new EvmAddress(order.refundAddress).unwrap()
+            : undefined;
+
+        const tx = await this.placeMarketOrderTx(
+            order.amountIn,
+            toChainId(order.targetChain),
+            order.redeemer.toUint8Array(),
+            msg,
+            order.minAmountOut,
+            refundAddress,
+        );
+
+        yield this.createUnsignedTx(tx, "TokenRouter.placeMarketOrder");
     }
     async *redeemFill(
-        sender: AccountAddress<C>,
+        sender: AnyEvmAddress,
         vaa:
             | VAA<"FastTransfer:CctpDeposit">
             | VAA<"FastTransfer:FastMarketOrder">
@@ -38,5 +60,22 @@ export class EvmTokenRouter<N extends Network, C extends Chain>
         cctp: CircleBridge.Attestation,
     ): AsyncGenerator<UnsignedTransaction<N, C>, any, unknown> {
         throw new Error("Method not implemented.");
+    }
+
+    private createUnsignedTx(
+        txReq: ethers.ContractTransaction,
+        description: string,
+        parallelizable: boolean = false,
+    ): EvmUnsignedTransaction<N, C> {
+        txReq.chainId = this._chainId;
+
+        return new EvmUnsignedTransaction(
+            // txReq,
+            {},
+            this.network,
+            this.chain,
+            description,
+            parallelizable,
+        );
     }
 }
