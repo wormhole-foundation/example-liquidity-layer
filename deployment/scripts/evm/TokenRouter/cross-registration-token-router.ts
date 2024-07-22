84ef0f860d9b61377c9ee44ffb80ed2c3047eba8
Agustin Velez <agustinmaurasvelez@gmail.com>
2024-07-22 14:05:14 -0300

    Finished cross registration script

--- deployment/scripts/evm/TokenRouter/cross-registration-token-router.ts
+++ deployment/scripts/evm/TokenRouter/cross-registration-token-router.ts
@@ -1,7 +1,7 @@
 import { ChainInfo, LoggerFn, getContractInstance, getContractAddress, runOnEvms, contracts, getUniversalAddress } from "../../../helpers";
 import { ethers } from "ethers";
 import { TokenRouter } from "../../../contract-bindings";
-import { circle } from "@wormhole-foundation/sdk-base";
+import { circle, toChain } from "@wormhole-foundation/sdk-base";
 
 runOnEvms("cross-registration-token-router", async (chain: ChainInfo, signer: ethers.Signer, log: LoggerFn) => {
   const tokenRouterAddress = getContractAddress("TokenRouterProxy", chain.chainId);
@@ -9,29 +9,25 @@ runOnEvms("cross-registration-token-router", async (chain: ChainInfo, signer: et
   const deployedTokenRouters = contracts['TokenRouterProxy'].filter((router) => router.chainId !== chain.chainId);
 
   for (const router of deployedTokenRouters) {
-    const circleDomain = circle.toCircleChainId(chain.network, router.chainId);
+    const circleDomain = circle.toCircleChainId(chain.network, toChain(router.chainId));
     const endpoint = {
       router: getUniversalAddress(router.address),
       mintRecipient: getUniversalAddress(router.address)
     };
 
-
-    // check if is already registered
-    
     if (router.chainId === 0) 
       throw new Error('Invalid chainId when register new router endpoint');
 
     if (Number(router.address) === 0)
       throw new Error(`Invalid router address for chainId ${router.chainId}`);
 
-    const isAlreadyRegistered = 
+    const currentMintRecipient = await tokenRouter.getMintRecipient(router.chainId);
+    if (Number(currentMintRecipient) !== 0) {
+      log(`Router endpoint already registered for chainId ${router.chainId}`);
+      continue;
+    }
 
     await tokenRouter.addRouterEndpoint(router.chainId, endpoint, circleDomain);
     log(`Router endpoint added for chainId ${router.chainId}`);
   }
 });
-
-function isRouterEndpointRegistered(router: TokenRouter, chainId: number, endpoint: { router: string; mintRecipient: string }) {
-  const mintRecipient = await tokenRouter.getMintRecipient(router.chainId, endpoint);
-  return Number(mintRecipient) !== 0;
-}
