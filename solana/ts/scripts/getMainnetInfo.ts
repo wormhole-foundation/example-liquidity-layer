771139646a5855e1398632bc468789e4c691e206
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-08-13 08:57:18 -0500

    ts/scripts: mainnet info

---
+++ solana/ts/scripts/getMainnetInfo.ts
@@ -0,0 +1,102 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+import { Chain, toChainId } from "@wormhole-foundation/sdk-base";
+import { toUniversal } from "@wormhole-foundation/sdk-definitions";
+import * as matchingEngineSdk from "../src/matchingEngine";
+import * as tokenRouterSdk from "../src/tokenRouter";
+
+const USDC_MINT = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+
+const CHAINS: Chain[] = ["Arbitrum", "Base"];
+
+// Here we go.
+main();
+
+// impl
+
+async function main() {
+    const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+
+    console.log("Collecting Solana Matching Engine and Token Router Info...");
+    console.log();
+    {
+        const matchingEngine = new matchingEngineSdk.MatchingEngineProgram(
+            connection,
+            matchingEngineSdk.mainnet(),
+            USDC_MINT,
+        );
+
+        const custodian = matchingEngine.custodianAddress();
+        console.log("Matching Engine");
+        console.log("  Custodian (NOTE: The Custodian's address is the program's emitter):");
+        console.log(`    Native:    ${custodian.toString()}`);
+        console.log(`    Universal: ${custodian.toBuffer().toString("hex")}`);
+        console.log();
+
+        const cctpMintRecipient = matchingEngine.cctpMintRecipientAddress();
+        console.log("  Mint Recipient:");
+        console.log(`    Native:    ${cctpMintRecipient.toString()}`);
+        console.log(`    Universal: ${cctpMintRecipient.toBuffer().toString("hex")}`);
+        console.log();
+
+        const custodianData = await matchingEngine.fetchCustodian();
+        console.log("Custodian Data");
+        console.log(JSON.stringify(custodianData, null, 2));
+        console.log();
+
+        const auctionConfig = await matchingEngine.fetchAuctionConfig(
+            custodianData.auctionConfigId,
+        );
+        console.log("Auction Config Data");
+        console.log(JSON.stringify(auctionConfig, null, 2));
+        console.log();
+
+        for (const chainName of CHAINS) {
+            await matchingEngine
+                .fetchRouterEndpointInfo(toChainId(chainName))
+                .then((endpointData) => {
+                    console.log(
+                        `Registered Endpoint (${chainName}): ${stringifyEndpoint(
+                            chainName,
+                            endpointData,
+                        )}`,
+                    );
+                })
+                .catch((err) => {
+                    console.log(`Not Registered: ${chainName}`);
+                });
+            console.log();
+        }
+    }
+
+    {
+        const tokenRouter = new tokenRouterSdk.TokenRouterProgram(
+            connection,
+            tokenRouterSdk.mainnet(),
+            USDC_MINT,
+        );
+
+        const custodian = tokenRouter.custodianAddress();
+        console.log(`Token Router`);
+        console.log("  Custodian (NOTE: The Custodian's address is the program's emitter):");
+        console.log(`    Native:    ${custodian.toString()}`);
+        console.log(`    Universal: ${custodian.toBuffer().toString("hex")}`);
+        console.log();
+
+        const cctpMintRecipient = tokenRouter.cctpMintRecipientAddress();
+        console.log("  Mint Recipient:");
+        console.log(`    Native:    ${cctpMintRecipient.toString()}`);
+        console.log(`    Universal: ${cctpMintRecipient.toBuffer().toString("hex")}`);
+        console.log();
+    }
+}
+
+function stringifyEndpoint(chain: Chain, endpoint: matchingEngineSdk.EndpointInfo) {
+    const out = {
+        address: toUniversal(chain, Uint8Array.from(endpoint.address)).toNative(chain).toString(),
+        mintRecipient: toUniversal(chain, Uint8Array.from(endpoint.mintRecipient))
+            .toNative(chain)
+            .toString(),
+        protocol: endpoint.protocol,
+    };
+    return JSON.stringify(out, null, 2);
+}
