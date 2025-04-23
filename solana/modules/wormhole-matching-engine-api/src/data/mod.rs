2e727bc46629c904748d86656bdde1685c09917e
Bengt Lofgren <bengt@wormholelabs.xyz>
2025-04-23 15:29:38 +0100

    removed newly added crate

--- solana/modules/wormhole-matching-engine-api/src/data/mod.rs
+++
@@ -1,8 +0,0 @@
-mod fast_market_order;
-mod initial_offer;
-mod order_response;
-
-// Re-export all data structures
-pub use fast_market_order::*;
-pub use initial_offer::*;
-pub use order_response::*;
--- solana/modules/wormhole-matching-engine-api/src/lib.rs
+++
@@ -1,6 +0,0 @@
-mod data;
-mod instructions;
-mod utils;
-
-pub use data::*;
-pub use instructions::*;
